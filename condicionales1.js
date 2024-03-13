aa14b577b0d617288cf60f74200722df5cfedf92
Maria <mfhastamorir@soy.sena.edu.co>
2024-03-12 22:31:14 -0500

    condicionales micelania

--- condicionales1.js
+++
@@ -1,9 +0,0 @@
-console.log("1. Escribir un algoritmo para saber si el número ingresado por teclado es positivo o negativo. ");
-console.log("Ingrese un número negativo o positivo: ");
-let num=11;
-
-if (num>0){
-    console.log("El número ingresado es positivo");
-}else{
-    console.log("El número ingresado es negativo");
-}
--- operadores2.js
+++
@@ -1,9 +0,0 @@
-console.log("2. Ingresar dos números por teclado y sumarlos: ");
-console.log("Ingrese número uno: ");
-let numuno=17;
-console.log("Ingrese número dos: ");
-let numdos=7;
-
-let resultado=(numuno+numdos);
-
-console.log(resultado);
